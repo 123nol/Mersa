22a878d64bba78dbb671b258843526cd565fc6f4
Shalom Arbsie <shalomarbsie00@gmail.com>
2026-01-28 05:30:33 +0300

    change for deployment error 15

--- it_company_task_manager/asgi.py
+++ it_company_task_manager/asgi.py
@@ -11,13 +11,8 @@ import os
 
 
 from django.core.asgi import get_asgi_application
-from whitenoise import WhiteNoise
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "it_company_task_manager.settings")
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
 django_asgi_app = get_asgi_application()
-application = WhiteNoise(django_asgi_app, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
-
-# Optional: enable gzip compression for faster delivery
-application.add_files(os.path.join(os.path.dirname(__file__), 'staticfiles'), prefix='static/')
